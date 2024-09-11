88d8fedff85f826c63c2c149580cb69b5ecd7a19
David Lavery <dlavery62@gmail.com>
2024-09-11 15:23:21 +0100

    improvements to the search algorithm and to the README

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # letters-cheat
-Cheat at the Countdown letter game
+Cheat at the Countdown letters game
 
 Impress your friends by cheating at Countdown...
+
+(Needs PostgreSQL to be downloaded and installed, https://www.postgresql.org/)
 
 Simply create a PostgreSQL database using the db-init.sql file in the db-migrations folder, then clone the git repo at https://github.com/InnovativeInventor/english-words.git to get a list of English words. Change the database.ini file to match your DB parameters and then load the english wordset into the DB using the loadwords python script. Run findwords.py to enter your 9 letters and get back a list of possible solutions.
 
@@ -11,9 +13,11 @@ And NOW...
 
 Cheat at Wordle!
 
-Add the new table to the wordlist database using the db-rawwords.sql file in the db-migrations folder then run loadrawwords.py to populate the table from the english-words list. Run findwordle.py where you will be asked to enter your attempts and the responses to them. Type "exit" or "quit" here to exit the program, type "done" once you have enterted your attempts to receive solution hints. Reponses should be coded as:
+Add the new table to the wordlist database using the db-rawwords.sql file in the db-migrations folder then run loadrawwords.py to populate the table from the english-words list. Run findwordle.py where you will be asked to enter your attempts and the responses to them. Type "exit" or "quit" here to exit the program, type "done" at the attempt prompt once you have entered your attempts to receive solution hints. Reponses should be coded as:
 y - correct letter in the correct place
 o - correct letter in the incorrect place
 n - incorrect letter
 
-e.g. if the word is "beast" and your attempt is "baker" then the response would be "yonon".
+e.g. if the word is "beast" and your attempt is "baker" then the response would be "yonon" (green, yellow, grey, yellow, grey on most apps/sites).
+
+You can also use this to suggest solutions when you're fairly sure you know the target word has a specific prefix or suffix. For example, if you're pretty certain a 9 letter wordle starts with "inter", you can enter "interxxxx" as your attempt and use "yyyyy...." as the response ("." is a neutral character and so doesn't affect the algorithm). You'll then receive a hint list that contains: interacts, interbred, etc.
--- findwordle.py
+++ findwordle.py
@@ -3,11 +3,17 @@ import psycopg2
 import config
 import sys
 
-def do_wordle(cfg:dict, attempts:str, debug:bool=False) -> list[tuple]:
-        conn = None
+def make_sql(attempts:list, debug:bool=False, trunc:int=0) -> str:
         present = []
         not_present = []
         pattn = ['^']
+        new_attempts = []
+        orig_len = len(attempts[0][0])
+        if trunc > 0:
+            trunc = trunc * -1
+            for a, r in attempts:
+                new_attempts.append((a[:trunc], r[:trunc]))
+            attempts = new_attempts
         for i in range(len(attempts[0][1])):
             pattn.append('.') 
         qry = "SELECT rawwords.word from " + cfg['dbname'] + ".rawwords WHERE "
@@ -50,31 +56,43 @@ def do_wordle(cfg:dict, attempts:str, debug:bool=False) -> list[tuple]:
             elif type(p) == list:
                 tmp = ''.join(p)
                 str_pattn = str_pattn + '[^' + tmp + ']'
-        if str_pattn[-1] == '.' or str_pattn[-1] == 's':    
-            # shorten matching pattern to accomodate plurals
-            str_pattn = str_pattn[:-1]
+        #if str_pattn[-1] == '.' or str_pattn[-1] == 's':    
+        #    # shorten matching pattern to accomodate plurals
+        #    str_pattn = str_pattn[:-1]
         qry = qry + "word ~ '" + str_pattn + "' "
         for p in present:
             qry = qry + "AND word ~ '" + p + "' "
         for n in not_present:
             qry = qry + "AND word !~ '" + n + "' "
         # include singular words in search where plural may be the answer
-        qry = qry + "AND length(word)>=" + str(len(attempts[0][0])-1) + " AND length(word)<=" + str(len(attempts[0][0])) + " "
-        qry = qry + "ORDER BY rawwords.word ASC LIMIT 20"
+        #qry = qry + "AND length(word)>=" + str(len(attempts[0][0])-1) + " AND length(word)<=" + str(len(attempts[0][0])) + " "
+        qry = qry + "AND length(word)>=" + str(len(attempts[0][0])) + " AND length(word)<=" + str(orig_len) + " "
+        qry = qry + "ORDER BY rawwords.word ASC LIMIT 25"
         if debug:
             print(qry)
+        return qry
+
+def do_wordle(cfg:dict, attempts:list, debug:bool=False) -> list[tuple]:
+        conn = None
         words = []
         try:
             # connect to the PostgreSQL server
             with psycopg2.connect(**cfg) as conn:
                 with conn.cursor() as cur:
-                    cur.execute(qry)
-                    rows = cur.fetchall()
-                    for r in rows:
-                        if len(r[0]) < len(attempts[0][0]):
-                            words.append(r[0]+'(s)')
-                        else:
+                    trunc = 0
+                    while True:
+                        qry = make_sql(attempts, debug, trunc)
+                        cur.execute(qry)
+                        rows = cur.fetchall()
+                        for r in rows:
+                            #if len(r[0]) < len(attempts[0][0]):
+                            #    words.append(r[0]+'(s)')
+                            #else:
+                            #    words.append(r[0])
                             words.append(r[0])
+                        trunc += 1
+                        if len(words) > 0 or trunc > 4:
+                            break
                     return(words)
         except (psycopg2.DatabaseError, Exception) as error:
             return (error)
@@ -94,6 +112,8 @@ if __name__ == '__main__':
             if attempt:
                 if attempts and len(attempt) != len(attempts[0][0]):
                     print("Please try again")
+                elif len(attempt) < 5:
+                    print("Too short - please try again")
                 else:
                     while True:
                         res = input("Enter the result: ").strip().lower()
@@ -106,9 +126,95 @@ if __name__ == '__main__':
         words = do_wordle(cfg, attempts, debug)
         print("-----------------")
         print("HINTS:")
-        for w in words:
-            print(w)
-        print("-----------------")
-        cont = input("Again (N)? ").strip().lower()
+        if type(words) == list:
+            for w in words:
+                print(w)
+            print("-----------------")
+            cont = input("Again (N)? ").strip().lower()
+        else:
+            print(words)
+            cont='n'
         if cont != 'y':
             break
+'''
+TESTS
+=====
+1.
+Inputs:
+Enter your attempt: juggernaut
+Enter the result: nynnonnnnn
+Enter your attempt: bumblebees
+Enter the result: nyonyynnny
+Enter your attempt: done
+
+Expected:
+cuddlesome
+huddledom
+muddledom
+muddlesome
+muffledly
+musclelike
+muzzlewood
+puzzledom
+
+2.
+Enter your attempt: vengeance
+Enter the result: noonnonyn
+Enter your attempt: ambulance
+Enter the result: ononnnoyo
+Enter your attempt: breakneck
+Enter the result: yooononyn
+Enter your attempt: done
+
+Expected:
+baronetcy
+
+3.
+Enter your attempt: juggernaut
+Enter the result: nonnnnnonn
+Enter your attempt: acquiesced
+Enter the result: ynooonnnnn
+Enter your attempt: done
+
+Expected:
+aquaphobia
+
+4.
+Enter your attempt: aquashapes 
+Enter the result: yyyyonnnon
+Enter your attempt: done
+
+Expected:
+aquacades
+aquatones
+
+5.
+Enter your attempt: stationer
+Enter the result: onnnoooon
+Enter your attempt: gestation
+Enter the result: noonnnooo
+Enter your attempt: splendour
+Enter the result: onooonoon
+Enter your attempt: unisexual
+Enter the result: oyooonnno
+Enter your attempt: inclusive
+Enter the result: oynooonoo
+Enter your attempt: done
+
+Expected:
+enviously
+
+6.
+Enter your attempt: juggernaut
+Enter the result: nynnnnnonn
+Enter your attempt: automation
+Enter the result: oynnnnnonn
+Enter your attempt: fundaments
+Enter the result: nynnonnnny
+Enter your attempt: supplicant
+Enter the result: oynnyooonn
+Enter your attempt: done
+
+Expected:
+quillbacks
+'''
